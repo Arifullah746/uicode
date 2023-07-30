e7da0862075a6ab84e32f1fff2a93178378d8280
Mohammad Arifulah Karim <marifullahk34@gmail.com>
2023-07-31 00:46:56 +0530

    varable

--- basic/variable.js
+++ basic/variable.js
@@ -3,7 +3,7 @@ let accountName="Arifullah" // changable
 
 var cityName="Bengaluru" // changable ,don't use because issue block scope, functional scope
 
-accountType="saving" // not for good practise
+accountType="saving" // not for good practise.
 
 
 console.log([accountId,accountName,cityName,accountType])
