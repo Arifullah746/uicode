2644d56df62b91554420e587265a2094486cfa2e
Mohammad Arifulah Karim <marifullahk34@gmail.com>
2023-07-31 00:45:02 +0530

    variable

---
+++ basic/variable.js
@@ -0,0 +1,9 @@
+const accountId="12345" // conn't change
+let accountName="Arifullah" // changable
+
+var cityName="Bengaluru" // changable ,don't use because issue block scope, functional scope
+
+accountType="saving" // not for good practise
+
+
+console.log([accountId,accountName,cityName,accountType])
